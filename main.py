aa083ec3443266a0de6dd2002939f9a7d73b2f23
Nicolas Cordoba <nico343@outlook.es>
2021-11-24 14:56:37 -0300

    home

--- main.py
+++ main.py
@@ -188,7 +188,22 @@ def update_user():
     tags=["Tweets"]
     )
 def home():
-    return {"Twitter Api": "Working"}
+    """
+    Show All Tweets
+
+    Parameters:
+        -
+    Returns a json list with all tweets in the app with the following keys:
+        tweet_id: UUID
+        content: str
+        created_at: datetime
+        updated_at: Optional[datetime] 
+        by: User
+    """
+    with open("tweets.json", "r", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        return results
+
 
 ### Post a tweet
 @app.post(
